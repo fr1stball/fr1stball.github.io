8067abcfc4a91db7832385f78a3ac955f3042161
fr1stball <khalikovartem000@gmail.com>
2025-12-21 16:32:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-
-app.get('/', (req, res) => res.send('Forest Server Running'));
+app.get('/', (req, res) => res.send('Forest Server v2 Running'));
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -24,32 +23,36 @@ io.on('connection', (socket) => {
         players[socket.id] = {
             id: socket.id,
             username: username || "Player",
-            socket: socket
+            socket: socket,
+            heroes: null // Сюда запишем выбор героев
         };
         socket.emit('loginSuccess', { username: players[socket.id].username });
     });
 
-    socket.on('findMatch', () => {
+    // Изменено: теперь принимаем героев при поиске
+    socket.on('findMatch', (heroData) => {
         const player = players[socket.id];
         if (!player) {
             socket.emit('loginSuccess', { username: "Guest" });
             return;
         }
+        
+        // Запоминаем выбор героев
+        player.heroes = heroData; // { p1: 'ranger', p2: 'bear' }
+
         if (matchmakingQueue.find(p => p.id === socket.id)) return;
 
         matchmakingQueue.push(player);
+        console.log(`[QUEUE] + ${player.username} (Heroes: ${heroData.p1}, ${heroData.p2})`);
+
         tryMatchmaking();
     });
 
-    // Пересылка выстрела (Начало хода)
     socket.on('shoot', (data) => {
         socket.to(data.room).emit('enemyShoot', data);
     });
 
-    // --- НОВОЕ: Пересылка координат после хода (Синхронизация) ---
     socket.on('syncTurn', (data) => {
-        // data содержит: { room, units: [...] }
-        // Отправляем это второму игроку, чтобы он выставил фигуры так же
         socket.to(data.room).emit('syncData', data.units);
     });
 
@@ -74,8 +77,24 @@ function tryMatchmaking() {
         p1.socket.join(roomName);
         p2.socket.join(roomName);
 
-        io.to(p1.id).emit('gameStart', { room: roomName, role: 'green', opponent: p2.username });
-        io.to(p2.id).emit('gameStart', { room: roomName, role: 'red', opponent: p1.username });
+        console.log(`[START] ${p1.username} vs ${p2.username}`);
+
+        // Отправляем каждому игроку информацию о враге
+        io.to(p1.id).emit('gameStart', { 
+            room: roomName, 
+            role: 'green', 
+            opponent: p2.username,
+            // Вражеские герои для P1 - это герои P2
+            enemyHeroes: p2.heroes 
+        });
+
+        io.to(p2.id).emit('gameStart', { 
+            room: roomName, 
+            role: 'red', 
+            opponent: p1.username,
+            // Вражеские герои для P2 - это герои P1
+            enemyHeroes: p1.heroes
+        });
     }
 }
 
