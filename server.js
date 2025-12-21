35169d06e015a871c02d92910619d8350638544b
fr1stball <khalikovartem000@gmail.com>
2025-12-21 16:49:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.send('Forest Server v2 Running'));
+app.get('/', (req, res) => res.send('Forest Server v3 Running'));
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -24,12 +24,12 @@ io.on('connection', (socket) => {
             id: socket.id,
             username: username || "Player",
             socket: socket,
-            heroes: null // Сюда запишем выбор героев
+            heroes: null,
+            currentRoom: null // Запоминаем комнату
         };
         socket.emit('loginSuccess', { username: players[socket.id].username });
     });
 
-    // Изменено: теперь принимаем героев при поиске
     socket.on('findMatch', (heroData) => {
         const player = players[socket.id];
         if (!player) {
@@ -37,13 +37,13 @@ io.on('connection', (socket) => {
             return;
         }
         
-        // Запоминаем выбор героев
-        player.heroes = heroData; // { p1: 'ranger', p2: 'bear' }
+        player.heroes = heroData;
 
+        // Если уже в очереди - не дублируем
         if (matchmakingQueue.find(p => p.id === socket.id)) return;
 
         matchmakingQueue.push(player);
-        console.log(`[QUEUE] + ${player.username} (Heroes: ${heroData.p1}, ${heroData.p2})`);
+        console.log(`[QUEUE] + ${player.username}`);
 
         tryMatchmaking();
     });
@@ -56,7 +56,19 @@ io.on('connection', (socket) => {
         socket.to(data.room).emit('syncData', data.units);
     });
 
+    // --- ОБРАБОТКА ВЫХОДА ---
     socket.on('disconnect', () => {
+        console.log(`[DISCONNECT] ${socket.id}`);
+        
+        const player = players[socket.id];
+        
+        // 1. Если игрок был в активной игре
+        if (player && player.currentRoom) {
+            // Сообщаем сопернику в этой комнате, что игра окончена
+            socket.to(player.currentRoom).emit('opponentLeft');
+        }
+
+        // 2. Удаляем из памяти
         delete players[socket.id];
         matchmakingQueue = matchmakingQueue.filter(p => p.id !== socket.id);
     });
@@ -74,26 +86,23 @@ function tryMatchmaking() {
         }
 
         const roomName = `battle_${p1.id}_${p2.id}`;
+        
+        // Подключаем к комнате Socket.IO
         p1.socket.join(roomName);
         p2.socket.join(roomName);
 
+        // Запоминаем комнату в объекте игрока (для дисконнекта)
+        p1.currentRoom = roomName;
+        p2.currentRoom = roomName;
+
         console.log(`[START] ${p1.username} vs ${p2.username}`);
 
-        // Отправляем каждому игроку информацию о враге
         io.to(p1.id).emit('gameStart', { 
-            room: roomName, 
-            role: 'green', 
-            opponent: p2.username,
-            // Вражеские герои для P1 - это герои P2
-            enemyHeroes: p2.heroes 
+            room: roomName, role: 'green', opponent: p2.username, enemyHeroes: p2.heroes 
         });
 
         io.to(p2.id).emit('gameStart', { 
-            room: roomName, 
-            role: 'red', 
-            opponent: p1.username,
-            // Вражеские герои для P2 - это герои P1
-            enemyHeroes: p1.heroes
+            room: roomName, role: 'red', opponent: p1.username, enemyHeroes: p1.heroes
         });
     }
 }
